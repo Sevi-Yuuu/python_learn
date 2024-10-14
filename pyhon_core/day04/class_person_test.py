c9b00d359757342ad5e0dcae7f2cbaf21b67e87a
tyistio <istio@qq.com>
2024-10-14 16:13:59 +0800

    feat : python基础 class person demo

---
+++ pyhon_core/day04/class_person_test.py
@@ -0,0 +1,54 @@
+class Person(object):
+    # 限定Person对象只能绑定_name, _age和_gender属性
+    __slots__ = ('_name', '_age', '_gender')
+
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    # 访问器 - getter方法
+    @property
+    def name(self):
+        return self._name
+
+    # 访问器 - getter方法
+    @property
+    def age(self):
+        return self._age
+
+    # 修改器 - setter方法
+    @age.setter
+    def age(self, age):
+        self._age = age
+
+    # 静态方法
+    @staticmethod
+    def is_valid(name, age):
+        if len(name) <= 0 or len(name) > 10:
+            return False
+        if age <= 0 or age > 100:
+            return False
+        return True
+
+    def play(self):
+        if self._age <= 16:
+            print('%s正在玩飞行棋.' % self._name)
+        else:
+            print('%s正在玩斗地主.' % self._name)
+
+
+def main():
+    name = '王大锤'
+    age = 122
+    if not Person.is_valid(name, age):
+        print('can not create person object!')
+        return
+    person = Person(name, age)
+    person.play()
+    person.age = 22
+    person.play()
+    # person.name = '白元芳'  # AttributeError: can't set attribute
+
+
+if __name__ == '__main__':
+    main()
